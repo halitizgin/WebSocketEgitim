485a2bb9bf4096da2b8018a92a59e20da4b898bd
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-13 00:15:36 +0300

    C# Websocket Ders 3 - on ve emit komutları

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ http.listen(3000);
 
 io.on('connection', socket => {
     console.log('Kullanıcı bağlandı.');
-
+    socket.on('veri', data => {
+        console.log(data);
+    });
     socket.on('disconnect', () => {
         console.log('Kullanıcı çıkış yaptı.');
     });
