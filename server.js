981d4d3514ca95041d8d5d27c6f5d4013cf2bdb2
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-13 00:40:03 +0300

    C# Websocket Ders 4 - socket.broadcast kullanımı

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ http.listen(3000);
 io.on('connection', socket => {
     console.log('Kullanıcı bağlandı.');
     socket.on('veri', data => {
-        console.log(data);
+        socket.broadcast.emit('veri', data);
     });
     socket.on('disconnect', () => {
         console.log('Kullanıcı çıkış yaptı.');
