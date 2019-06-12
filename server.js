47f2edd550208cdce76571a90cf63d66afe95de3
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-12 15:49:06 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+
+http.listen(3000);
+
+io.on('connection', socket => {
+    console.log('Kullanıcı bağlandı.');
+
+    socket.on('disconnect', () => {
+        console.log('Kullanıcı çıkış yaptı.');
+    });
+});
