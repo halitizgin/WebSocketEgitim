5e6b58c95c3d08b928da39900b64870ceaa14ba8
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-16 01:29:33 +0300

    C# Websocket Ders 5 - Aktif kullanıcı listesi alma işlemleri

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const io = require('socket.io')(http);
 
 http.listen(3000);
 
+const users = [];
+
 io.on('connection', socket => {
     console.log('Kullanıcı bağlandı.');
-    socket.on('veri', data => {
-        socket.broadcast.emit('veri', data);
+    
+    socket.on('name', name => {
+        socket.name = name;
+        users.push(name);
+        socket.emit('users', users);
+        socket.broadcast.emit('addUser', name);
     });
+
     socket.on('disconnect', () => {
+        const index = users.indexOf(socket.name);
+        if (index !== -1){
+            users.splice(index, 1);
+        }
+        socket.broadcast.emit('deleteUser', socket.name);
         console.log('Kullanıcı çıkış yaptı.');
     });
 });
