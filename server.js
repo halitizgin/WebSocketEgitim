fa30e080c7bb5b12f0665918ebc0aacdd18fe974
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-17 23:29:08 +0300

    C# Websocket Ders 6 - Belirli kullanıcıya mesaj gönderme

--- server.js
+++ server.js
@@ -7,20 +7,24 @@ const users = [];
 
 io.on('connection', socket => {
     console.log('Kullanıcı bağlandı.');
-    
     socket.on('name', name => {
+        const user = { username: name, id: socket.id };
         socket.name = name;
-        users.push(name);
+        users.push(user);
         socket.emit('users', users);
-        socket.broadcast.emit('addUser', name);
+        socket.broadcast.emit('addUser', user);
+    });
+
+    socket.on('pm', data => {
+        io.sockets.to(data.to).emit('pm', data);
     });
 
     socket.on('disconnect', () => {
-        const index = users.indexOf(socket.name);
+        const index = users.findIndex(item => item.id === socket.id);
         if (index !== -1){
             users.splice(index, 1);
         }
-        socket.broadcast.emit('deleteUser', socket.name);
+        socket.broadcast.emit('deleteUser', { id: socket.id, username: socket.name });
         console.log('Kullanıcı çıkış yaptı.');
     });
 });
